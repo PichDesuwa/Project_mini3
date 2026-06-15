eefb312752bb82a9830c94771b5591b0075f3e63
SOPHANITH <yoongsophanith18@gmail.com>
2026-06-15 15:37:26 +0700

    hh

---
+++ Controller.java
@@ -0,0 +1,29 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
+
+public class Controller {
+
+    @FXML
+    private ScrollPane scrollpane;
+
+    @FXML
+    private Button submit;
+
+    @FXML
+    private Label total;
+
+    @FXML
+    void On_submit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_scroll(MouseEvent event) {
+
+    }
+
+}
